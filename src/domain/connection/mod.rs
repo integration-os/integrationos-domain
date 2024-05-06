6e029ab5f51113fb88bb77ed533e47cfca865457
Samuel <39674930+samgj18@users.noreply.github.com>
2024-05-06 17:40:00 +0100

    Removing sensitive data from connection configuration (#19)

--- src/domain/connection/mod.rs
+++ src/domain/connection/mod.rs
@@ -5,12 +5,11 @@ pub mod connection_model_schema;
 pub mod connection_oauth_definition;
 pub mod db_model_config;
 
-use std::{hash::Hash, sync::Arc};
-
-use serde::{Deserialize, Serialize};
-use strum::{AsRefStr, Display, EnumString};
-
 use crate::id::Id;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::{hash::Hash, sync::Arc};
+use strum::{AsRefStr, Display, EnumString};
 
 use self::{
     connection_model_definition::ConnectionModelDefinition,
@@ -50,6 +49,36 @@ pub struct Connection {
     pub oauth: Option<OAuth>,
     #[serde(flatten, default)]
     pub record_metadata: RecordMetadata,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SanitizedConnection {
+    #[serde(rename = "_id")]
+    pub id: Id,
+    pub platform_version: String,
+    pub connection_definition_id: Id,
+    pub r#type: ConnectionType,
+    pub name: String,
+    pub key: Arc<str>,
+    pub group: String,
+    pub environment: Environment,
+    pub platform: Arc<str>,
+    pub secrets_service_id: String,
+    pub event_access_id: Id,
+    pub settings: Settings,
+    pub throughput: Throughput,
+    pub ownership: Ownership,
+    #[serde(default)]
+    pub oauth: Option<OAuth>,
+    #[serde(flatten, default)]
+    pub record_metadata: RecordMetadata,
+}
+
+impl SanitizedConnection {
+    pub fn to_value(&self) -> Value {
+        serde_json::to_value(self).unwrap_or(Value::Null)
+    }
 }
 
 impl Hash for Connection {
